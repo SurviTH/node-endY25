e750ea7f15bfae261c4812f2f2c836f8279a23bf
Kornsrikranet Tanasrithatchai <k@Kornsrikranets-MacBook-Pro.local>
2025-12-19 21:08:10 +0700

    Update Content

---
+++ global/global.js
@@ -0,0 +1,16 @@
+// Log Content of the global object
+// console.log(global);
+
+// Accessing global properties
+// global.setTimeout(() => {
+//     console.log("This message is displayed after a delay.");
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log("This message is displayed every 2 seconds.");
+// }, 2000);
+
+// Log directory name and file name
+console.log(__dirname);
+console.log(__filename);
